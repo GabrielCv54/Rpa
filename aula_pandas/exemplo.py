33b8f492f3e275a28ce5cca1c402b4c900722ae6
Gabriel <gabriel.carvalhovieira123@gmail.com>
2025-09-11 15:55:28 -0300

    aula introdutória sobre o pandas

--- aula_pandas/exemplo.py
+++ aula_pandas/exemplo.py
@@ -1,6 +1,13 @@
-#LImpeza de dados
+#Limpeza de dados
 
+from openpyxl import load_workbook
 import pandas as pd
 
-df = pd.read_excel('livros atualizados.xlsx')
-print(df)
+df = pd.read_excel('aula_pandas/Boletim.xlsx')
+wb = load_workbook('aula_pandas/Boletim.xlsx')
+ws = wb.active
+ws["A3"] = 'Joana Figueiredo'
+
+wb.save('aula_pandas/Boletim_atualizados.xlsx')
+
+print(wb)
